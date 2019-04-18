b764c05a05b13c6c0bb61f0589707a0e44e37be2
Camila Maia <cmaiacd@gmail.com>
2019-04-18 21:02:49 +0200

    Initial swift project structure

---
+++ GoGerman/GoGerman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoGerman
+//
+//  Created by Camila Maia on 18/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
